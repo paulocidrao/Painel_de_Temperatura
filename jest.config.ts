c1f925473b2581f535c4ab499225ae92fb35e16d
Paulo Cidrão Gomes Torres <103290791+paulo-cidrao@users.noreply.github.com>
2024-06-11 14:55:06 -0300

    feat:add test config

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^.+\\.svg$': 'jest-transformer-svg',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
